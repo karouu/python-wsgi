7c51445ed15b053f8fdb6a7da50253ddb1d4e645
karouu <chenda721@gmail.com>
2019-07-08 17:35:01 +0800

    add counter using cookie and set_cookie

--- bottle/hello.py
+++ bottle/hello.py
@@ -4,4 +4,12 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
+from bottle import request, response
+@route('/counter')
+def counter():
+    count = int( request.cookies.get('counter', '0') )
+    count += 1
+    response.set_cookie('counter', str(count))
+    return 'You visited this page %d times' % count
+
 run(host='0.0.0.0', port=8080)
