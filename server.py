8193d30d007d6550d9d3e37c6b98614ca0217b81
karouu <chenda721@gmail.com>
2019-07-09 21:14:27 +0800

    deprecate with...as... clause

--- server.py
+++ server.py
@@ -3,7 +3,11 @@ import socketserver
 
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
-
+'''
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
+'''
+httpd = socketserver.TCPServer(("",PORT), Handler)
+print("servig at port", PORT)
+httpd.serve_forever()
