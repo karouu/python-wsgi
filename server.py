e6195fe51bce3619e666f83da85408148257ece3
karouu <chenda721@gmail.com>
2019-07-10 21:25:52 +0800

    webserver2,wsgi implemention

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
+#run in python3
 import http.server
 import socketserver
 
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
+
 '''
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
 '''
+
 httpd = socketserver.TCPServer(("",PORT), Handler)
 print("servig at port", PORT)
 httpd.serve_forever()
