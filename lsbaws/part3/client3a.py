b4830b7e44816d843df3f952403c039416b13ae7
karouu <chenda721@gmail.com>
2019-07-12 21:37:36 +0800

    sock fileno

--- lsbaws/part3/client3a.py
+++ lsbaws/part3/client3a.py
@@ -1,10 +1,12 @@
- import socket
+import socket
+import os
+import sys
 
- # create a socket and connect to a server
- sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
- sock.connect(('localhost', 8888))
-
- # send and receive some data
- sock.sendall(b'test')
- data = sock.recv(1024)
- print(data.decode())
+# create a socket and connect to a server
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('localhost', 8888))
+print("sock fileno is: ",os.write(sys.stdout.fileno(),sock.fileno()))
+# send and receive some data
+sock.sendall(b'test')
+data = sock.recv(1024)
+print(data.decode())
