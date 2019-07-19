a3e6aad1f551d4affef64624a6810613c8a7fc78
karouu <chenda721@gmail.com>
2019-07-19 09:43:01 +0800

    review 0719

--- LetsBuildAWebServer/server.py
+++ LetsBuildAWebServer/server.py
@@ -3,7 +3,7 @@ import http.server
 import socketserver
 
 PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+Handler = http.server.SimpleHTTPRequestHandler # handle with index.html
 
 '''
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
